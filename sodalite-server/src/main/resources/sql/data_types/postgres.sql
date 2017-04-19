b74b8c3662ad4afb48105f4c8ac44654100aedab
Kevin Hartmann <Kevin.Hartmann@oicr.on.ca>
2017-04-19 18:24:28 -0400

    Bugfix: We drop the data types for postgres only if they already exist. Enhancement: The stub file, create_view.sql,  now contains a single view, detail, that shows most of the important information associated with a file.

--- sodalite-server/src/main/resources/sql/data_types/postgres.sql
+++ sodalite-server/src/main/resources/sql/data_types/postgres.sql
@@ -1,8 +1,8 @@
-DROP TYPE gender;
+DROP TYPE IF EXISTS gender;
 CREATE TYPE gender as ENUM('Male','Female','Unknown');
-DROP TYPE specimen_class;
+DROP TYPE IF EXISTS specimen_class;
 CREATE TYPE specimen_class as ENUM('Normal','Tumour','Adjacent normal');
-DROP TYPE specimen_type;
+DROP TYPE IF EXISTS specimen_type;
 CREATE TYPE specimen_type as ENUM('Normal-solid tissue',
     'Normal - blood derived', 'Normal - bone marrow',
     'Normal - tissue adjacent to primary', 'Normal - buccal cell',
@@ -23,11 +23,11 @@ CREATE TYPE specimen_type as ENUM('Normal-solid tissue',
     'Xenograft - derived from tumour cell line',
     'Cell line - derived from tumour', 'Primary tumour - lymph node',
     'Metastatic tumour - other', 'Cell line - derived from xenograft tumour');
-DROP TYPE sample_type;
+DROP TYPE IF EXISTS sample_type;
 CREATE TYPE sample_type as ENUM('DNA','FFPE DNA','Amplified DNA','RNA','Total RNA','FFPE RNA');
-DROP TYPE file_type;
+DROP TYPE IF EXISTS file_type;
 CREATE TYPE file_type as ENUM('FASTA','FAI','FASTQ','BAM','BAI','VCF','TBI','IDX');
-DROP TYPE analysis_state;
+DROP TYPE IF EXISTS analysis_state;
 CREATE TYPE analysis_state as ENUM('Created','Uploaded','Published','Suppressed');
-DROP TYPE library_strategy;
+DROP TYPE IF EXISTS library_strategy;
 CREATE TYPE library_strategy as ENUM('WGS','WXS','RNA-Seq','ChIP-Seq','miRNA-Seq','Bisulfite-Seq','Validation','Amplicon','Other');
