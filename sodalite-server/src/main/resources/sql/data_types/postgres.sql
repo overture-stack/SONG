3b744813a946faf48d36e9f468333b3cc8cf741b
Kevin Hartmann <Kevin.Hartmann@oicr.on.ca>
2017-04-19 17:52:15 -0400

    Bugfix: Restored copyright message to schema.sql Bugfix: Drop existing data type before creating new version in data_types/postgres.sql

--- sodalite-server/src/main/resources/sql/data_types/postgres.sql
+++ sodalite-server/src/main/resources/sql/data_types/postgres.sql
@@ -1,5 +1,8 @@
+DROP TYPE gender;
 CREATE TYPE gender as ENUM('Male','Female','Unknown');
+DROP TYPE specimen_class;
 CREATE TYPE specimen_class as ENUM('Normal','Tumour','Adjacent normal');
+DROP TYPE specimen_type;
 CREATE TYPE specimen_type as ENUM('Normal-solid tissue',
     'Normal - blood derived', 'Normal - bone marrow',
     'Normal - tissue adjacent to primary', 'Normal - buccal cell',
@@ -20,7 +23,11 @@ CREATE TYPE specimen_type as ENUM('Normal-solid tissue',
     'Xenograft - derived from tumour cell line',
     'Cell line - derived from tumour', 'Primary tumour - lymph node',
     'Metastatic tumour - other', 'Cell line - derived from xenograft tumour');
+DROP TYPE sample_type;
 CREATE TYPE sample_type as ENUM('DNA','FFPE DNA','Amplified DNA','RNA','Total RNA','FFPE RNA');
-create TYPE file_type as ENUM('FASTA','FAI','FASTQ','BAM','BAI','VCF','TBI','IDX');
+DROP TYPE file_type;
+CREATE TYPE file_type as ENUM('FASTA','FAI','FASTQ','BAM','BAI','VCF','TBI','IDX');
+DROP TYPE analysis_state;
 CREATE TYPE analysis_state as ENUM('Created','Uploaded','Published','Suppressed');
+DROP TYPE library_strategy;
 CREATE TYPE library_strategy as ENUM('WGS','WXS','RNA-Seq','ChIP-Seq','miRNA-Seq','Bisulfite-Seq','Validation','Amplicon','Other');
