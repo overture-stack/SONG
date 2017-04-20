4878c27f9e448551e0ef45358cd80a1a7fc8f056
Kevin Hartmann <Kevin.Hartmann@oicr.on.ca>
2017-04-20 16:45:56 -0400

    Bugfix: Added CASCADE to drop the enumerated data types Change: Remove the 'info' field from all the table definitions and test data.

--- sodalite-server/src/main/resources/sql/data_types/postgres.sql
+++ sodalite-server/src/main/resources/sql/data_types/postgres.sql
@@ -1,8 +1,8 @@
-DROP TYPE IF EXISTS gender;
+DROP TYPE IF EXISTS gender CASCADE;
 CREATE TYPE gender as ENUM('Male','Female','Unknown');
-DROP TYPE IF EXISTS specimen_class;
+DROP TYPE IF EXISTS specimen_class CASCADE;
 CREATE TYPE specimen_class as ENUM('Normal','Tumour','Adjacent normal');
-DROP TYPE IF EXISTS specimen_type;
+DROP TYPE IF EXISTS specimen_type CASCADE;
 CREATE TYPE specimen_type as ENUM('Normal-solid tissue',
     'Normal - blood derived', 'Normal - bone marrow',
     'Normal - tissue adjacent to primary', 'Normal - buccal cell',
@@ -23,11 +23,11 @@ CREATE TYPE specimen_type as ENUM('Normal-solid tissue',
     'Xenograft - derived from tumour cell line',
     'Cell line - derived from tumour', 'Primary tumour - lymph node',
     'Metastatic tumour - other', 'Cell line - derived from xenograft tumour');
-DROP TYPE IF EXISTS sample_type;
+DROP TYPE IF EXISTS sample_type CASCADE;
 CREATE TYPE sample_type as ENUM('DNA','FFPE DNA','Amplified DNA','RNA','Total RNA','FFPE RNA');
-DROP TYPE IF EXISTS file_type;
+DROP TYPE IF EXISTS file_type CASCADE;
 CREATE TYPE file_type as ENUM('FASTA','FAI','FASTQ','BAM','BAI','VCF','TBI','IDX');
-DROP TYPE IF EXISTS analysis_state;
+DROP TYPE IF EXISTS analysis_state CASCADE;
 CREATE TYPE analysis_state as ENUM('Created','Uploaded','Published','Suppressed');
-DROP TYPE IF EXISTS library_strategy;
+DROP TYPE IF EXISTS library_strategy CASCADE;
 CREATE TYPE library_strategy as ENUM('WGS','WXS','RNA-Seq','ChIP-Seq','miRNA-Seq','Bisulfite-Seq','Validation','Amplicon','Other');
