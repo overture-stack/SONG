727335412f6df9de379d4edbd144a33dbf91350f
khartmann <kevin.hartmann@oicr.on.ca>
2017-06-07 15:02:25 -0400

    Enhancement: Improved the singTest.sh script; fixed bug where jq wasn't showing the right field ("status" vs. "state")

--- singTest.sh
+++ singTest.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+set -x 
 u=`sing upload -f metadata.json`
-sing status -u $u | jq .status
+sing status -u $u | jq -C .state 
+sleep 1
+sing status -u $u | jq -C .state 
 a=`sing save -u $u`
+sing status -u $u | jq -C .state 
 sing manifest -a $a -f manifest.txt
+cat manifest.txt
