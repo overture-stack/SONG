3ea313419aad41cfd0724d11acc9b0af1b7687f0
rtisma <rtisma@gmail.com>
2017-06-07 11:22:17 -0400

    updated

--- sodalite-server/src/main/java/org/icgc/dcc/sodalite/server/service/ExistenceService.java
+++ sodalite-server/src/main/java/org/icgc/dcc/sodalite/server/service/ExistenceService.java
@@ -3,11 +3,6 @@ package org.icgc.dcc.sodalite.server.service;
 import lombok.NonNull;
 import lombok.SneakyThrows;
 import lombok.val;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
-import org.springframework.retry.RetryCallback;
-import org.springframework.retry.RetryContext;
-import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
@@ -19,12 +14,15 @@ import static java.lang.Boolean.parseBoolean;
 import static org.icgc.dcc.common.core.util.Joiners.SLASH;
 
 @Service
-@Profile("secure")
 public class ExistenceService {
+
   private static final String UPLOAD = "upload";
 
-  @Autowired
-  private RetryTemplate retryTemplate;
+  /**
+   * [JIRA]: DCC-5630 - Add Retry feature for ExistenceService
+   * @Autowired
+   * private RetryTemplate retryTemplate;
+   */
 
   @NonNull private final String storageUrl;
 
@@ -34,8 +32,9 @@ public class ExistenceService {
 
   @SneakyThrows
   public boolean isObjectExist(String accessToken, String objectId) {
-    return retryTemplate.execute(new RetryCallback<Boolean, Throwable>() {
-      @Override public Boolean doWithRetry(RetryContext retryContext) throws Throwable {
+//    [JIRA]: DCC-5630 - Add Retry feature for ExistenceService
+//    return retryTemplate.execute(new RetryCallback<Boolean, Throwable>() {
+//      @Override public Boolean doWithRetry(RetryContext retryContext) throws Throwable {
         val endpointUrl = joinUrl(storageUrl, objectId);
         val url = new URL(endpointUrl);
         val con = (HttpURLConnection)url.openConnection();
@@ -45,8 +44,9 @@ public class ExistenceService {
             .lines()
             .findFirst();
         return parseBoolean(opt.orElseThrow( () ->  new IllegalStateException("There was no response. Expecting a [true] or [false]")));
-      }
-    });
+//    [JIRA]: DCC-5630 - Add Retry feature for ExistenceService
+//      }
+//    });
   }
 
   @SneakyThrows
--- sodalite-server/src/test/java/org/icgc/dcc/sodalite/server/service/ExistenceServiceTest.java
+++ sodalite-server/src/test/java/org/icgc/dcc/sodalite/server/service/ExistenceServiceTest.java
@@ -3,27 +3,22 @@ package org.icgc.dcc.sodalite.server.service;
 import lombok.SneakyThrows;
 import lombok.val;
 import org.assertj.core.api.Assertions;
+import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
-@SpringBootTest
-@RunWith(SpringRunner.class)
-@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class})
-@ActiveProfiles("secure")
+import static java.lang.System.getProperty;
+import static java.util.Objects.requireNonNull;
+
 public class ExistenceServiceTest {
   private static final String BASE_URL = "https://storage.cancercollaboratory.org";
   private static final String OBJ_ID = "cecb35d8-2b3b-5cf6-a775-24eada1c4651";
 
   @Test
   @SneakyThrows
+  @Ignore
   public void testGet(){
     val exi = ExistenceService.createExistenceService(BASE_URL);
-    val accessToken = "6634c87d-85bb-4ff7-9259-cf57bddf87b0-";
+    val accessToken = requireNonNull(getProperty("token"));
     Assertions.assertThat(exi.isObjectExist(accessToken,OBJ_ID)).isTrue();
   }
 
