66b24eb5b95f130a0cf26b9b3da330d4f58c4eb7
khartmann <kevin.hartmann@oicr.on.ca>
2017-06-12 10:15:22 -0400

    Bugfix: Fixed quoting bug in singTest.sh

--- singTest.sh
+++ singTest.sh
@@ -6,7 +6,7 @@ sing status -u $u | jq -C .state
 sleep 1
 sing status -u $u | jq -C .state 
 a=`sing save -u $u`
-echo "Got analysis id '$a''
+echo "Got analysis id '$a'"
 sing status -u $u | jq -C .state 
 sing manifest -a $a -f manifest.txt
 cat manifest.txt
