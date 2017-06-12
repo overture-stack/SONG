c23f9e2ebddacf28841d4eaa335bacac676283ae
rtisma <rtisma@gmail.com>
2017-06-12 01:15:21 -0400

    updateeed

--- song-server/src/main/java/org/icgc/dcc/song/server/service/ExistenceService.java
+++ song-server/src/main/java/org/icgc/dcc/song/server/service/ExistenceService.java
@@ -1,8 +1,10 @@
 package org.icgc.dcc.song.server.service;
 
 import lombok.NonNull;
+import lombok.Setter;
 import lombok.SneakyThrows;
 import lombok.val;
+import org.icgc.dcc.common.core.util.Joiners;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Service;
@@ -13,39 +15,53 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 import static java.lang.Boolean.parseBoolean;
-import static org.icgc.dcc.common.core.util.Joiners.SLASH;
+import static lombok.AccessLevel.PRIVATE;
 
 @Service
 public class ExistenceService {
 
   private static final String UPLOAD = "upload";
 
+  private static final int DEFAULT_TIMEOUT = 2;
+
   @Autowired
   private RetryTemplate retryTemplate;
 
   @NonNull private final String storageUrl;
 
+  @Setter(PRIVATE)
+  private int timeout;
+
   public ExistenceService(RetryTemplate retryTemplate, String storageUrl) {
     this.storageUrl = joinUrl(storageUrl, UPLOAD);
     this.retryTemplate = retryTemplate;
+    setTimeout(DEFAULT_TIMEOUT);
   }
 
   @SneakyThrows
   public boolean isObjectExist(@NonNull String accessToken, @NonNull String objectId) {
     return retryTemplate.execute(retryContext -> {
-        val endpointUrl = joinUrl(storageUrl, objectId);
-        val url = new URL(endpointUrl);
-        val con = (HttpURLConnection)url.openConnection();
-        con.setRequestMethod("GET");
-        con.setRequestProperty("Authorization", accessToken);
-        val opt = getReader(con)
-            .lines()
-            .findFirst();
-        return parseBoolean(opt.orElseThrow( () ->  new IllegalStateException("There was no response. Expecting a [true] or [false]")));
+      val endpointUrl = joinUrl(storageUrl, objectId);
+      val url = new URL(endpointUrl);
+      val con = (HttpURLConnection)url.openConnection();
+      con.setRequestMethod("GET");
+      con.setRequestProperty("Authorization", accessToken);
+      con.setConnectTimeout(timeout);
+      con.setReadTimeout(timeout);
+      val opt = getReader(con)
+          .lines()
+          .findFirst();
+      return parseBoolean(opt.orElseThrow( () ->  new IllegalStateException("There was no response. Expecting a [true] or [false]")));
     });
   }
 
-  @SneakyThrows
+
+  public static ExistenceService createExistenceService(RetryTemplate retryTemplate, String storageUrl, int timeout){
+    val e = createExistenceService(retryTemplate, storageUrl);
+    e.setTimeout(timeout);
+    return e;
+  }
+
   public static ExistenceService createExistenceService(RetryTemplate retryTemplate,String baseUrl){
     return new ExistenceService(retryTemplate,baseUrl);
   }
@@ -56,7 +72,7 @@ public class ExistenceService {
   }
 
   private static String joinUrl(String ... path){
-    return SLASH.join(path);
+    return Joiners.SLASH.join(path);
   }
 
 }
--- song-server/src/test/java/org/icgc/dcc/song/server/service/ExistenceServiceTest.java
+++ song-server/src/test/java/org/icgc/dcc/song/server/service/ExistenceServiceTest.java
@@ -8,7 +8,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
+import org.assertj.core.util.Lists;
+import org.assertj.core.util.Sets;
 import org.icgc.dcc.song.server.config.RetryConfig;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,9 +25,18 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.LinkedHashSet;
+import java.util.List;
 
 import static java.lang.String.format;
+import static java.lang.Thread.sleep;
+import static lombok.AccessLevel.PRIVATE;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.util.Sets.newLinkedHashSet;
+import static org.icgc.dcc.song.server.service.ExistenceService.createExistenceService;
+import static org.icgc.dcc.song.server.service.ExistenceServiceTest.MockDccStorageHandler.createFailingMockDccStorageHandler;
+import static org.icgc.dcc.song.server.service.ExistenceServiceTest.MockDccStorageHandler.createNormalMockDccStorageHandler;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -36,52 +48,115 @@ public class ExistenceServiceTest {
   @Autowired
   private RetryTemplate retryTemplate;
 
+  private static final int PORT = 8080;
+  private static final String HOSTNAME = "localhost";
+  private static final String MOCK_SERVER_URL = format("http://%s:%s",HOSTNAME, PORT);
+  private static final String OBJECT_ID1 = "qwertyObjectId1";
+  private static final String OBJECT_ID2 = "qwertyObjectId2";
+  private static final String TOKEN = "12345678";
+  private static final int REQUEST_CON_TIMEOUT = 2;
+  private static final long MS_UNTIL_KILL = (REQUEST_CON_TIMEOUT + 1)*1000L;
+
+  private HttpServer mockServer;
+
+  @Before
+  @SneakyThrows
+  public void beforeTest(){
+    this.mockServer = HttpServer.create(new InetSocketAddress(PORT), 0);
+  }
+
   @Test
   @SneakyThrows
   public void testIsObjectExists(){
-    val hostname = "localhost";
-    val port = 8080;
-    val mockServerUrl = format("http://%s:%s",hostname, port);
-
-    val objectId1 = "qwertyObjectId1";
-    val objectId2 = "qwertyObjectId2";
-    val token = "12345678";
 
     //Create HttpServer that mocks dcc-storage /upload endpoint
-    val mockServer = HttpServer.create(new InetSocketAddress(port), 0);
-    mockServer.createContext(format("/upload/%s",objectId1),new MockDccStorageHandler(true, token));
-    mockServer.createContext(format("/upload/%s",objectId2),new MockDccStorageHandler(false, token));
+    mockServer.createContext(format("/upload/%s",OBJECT_ID1), createNormalMockDccStorageHandler(true, TOKEN));
+    mockServer.createContext(format("/upload/%s",OBJECT_ID2), createNormalMockDccStorageHandler(false, TOKEN ));
     mockServer.start();
 
-    val exi = ExistenceService.createExistenceService(retryTemplate,mockServerUrl);
-    assertThat(exi.isObjectExist(token,objectId1)).isTrue();
-    assertThat(exi.isObjectExist(token,objectId2)).isFalse();
+    val exi = createExistenceService(retryTemplate,MOCK_SERVER_URL, REQUEST_CON_TIMEOUT);
+    assertThat(exi.isObjectExist(TOKEN,OBJECT_ID1)).isTrue();
+    assertThat(exi.isObjectExist(TOKEN,OBJECT_ID2)).isFalse();
     val seconds = 1;
     mockServer.stop(seconds);
   }
 
-  @RequiredArgsConstructor
+  @Test
+  @SneakyThrows
+  public void testExistanceRetryKilledOnce(){
+    val handler = createFailingMockDccStorageHandler(true, TOKEN, MS_UNTIL_KILL);
+    runFailTest(handler);
+  }
+
+  @Test
+  @SneakyThrows
+  public void testExistanceRetryKilled0123(){
+    val killList = Lists.newArrayList(0,1,2,3);
+    val handler = createFailingMockDccStorageHandler(true, TOKEN, MS_UNTIL_KILL, killList);
+    runFailTest(handler);
+  }
+
+  private void runFailTest(MockDccStorageHandler handler){
+    //Create HttpServer that mocks dcc-storage /upload endpoint
+    mockServer.createContext(format("/upload/%s",OBJECT_ID1), handler);
+    mockServer.start();
+    val exi = createExistenceService(retryTemplate,MOCK_SERVER_URL, REQUEST_CON_TIMEOUT);
+    assertThat(exi.isObjectExist(TOKEN,OBJECT_ID1)).isTrue();
+    val seconds = 1;
+    mockServer.stop(seconds);
+  }
+
+  @RequiredArgsConstructor(access =  PRIVATE)
   @Slf4j
   public static class MockDccStorageHandler implements HttpHandler{
 
     private final boolean result;
     @NonNull private final String expectedToken;
+    @NonNull private final HttpStatus errorStatus;
+    private final long msUntilKill;
+    private final LinkedHashSet<Integer> retryNumbersToKill;
 
+    private int retryNum = 0;
+
+
+    @SneakyThrows
     @Override public void handle(HttpExchange httpExchange) throws IOException {
       val headers = httpExchange.getRequestHeaders();
       val actualToken = headers.get(HttpHeaders.AUTHORIZATION).get(0);
-      if (actualToken.equals(expectedToken)){
-        val response = Boolean.toString(result);
-        httpExchange.sendResponseHeaders(HttpStatus.OK.value(), response.length());
-        val os = httpExchange.getResponseBody();
-        os.write(response.getBytes());
-        os.close();
-      } else {
-        httpExchange.sendResponseHeaders(HttpStatus.UNAUTHORIZED.value(), 0);
+      val shouldKill = retryNumbersToKill.contains(retryNum);
+      if (shouldKill) {
+        sleep(msUntilKill);
+      }else {
+        if (actualToken.equals(expectedToken)){
+          val response = Boolean.toString(result);
+          httpExchange.sendResponseHeaders(HttpStatus.OK.value(), response.length());
+          val os = httpExchange.getResponseBody();
+          os.write(response.getBytes());
+          os.close();
+        } else {
+          httpExchange.sendResponseHeaders(HttpStatus.UNAUTHORIZED.value(), 0);
+        }
       }
+      ++retryNum;
     }
 
-  }
+    public static MockDccStorageHandler createFailingMockDccStorageHandler(boolean result,
+        String expectedToken, long msUntilKill) {
+      return new MockDccStorageHandler(result, expectedToken,INTERNAL_SERVER_ERROR, msUntilKill, newLinkedHashSet(0));
+    }
 
+    public static MockDccStorageHandler createFailingMockDccStorageHandler(boolean result,
+        String expectedToken, long msUntilKill, List<Integer> retryNumbersToKill) {
+
+      val set = Sets.<Integer>newLinkedHashSet();
+      set.addAll(retryNumbersToKill);
+      return new MockDccStorageHandler(result, expectedToken, INTERNAL_SERVER_ERROR, msUntilKill, set);
+    }
+
+    public static MockDccStorageHandler createNormalMockDccStorageHandler(boolean result,
+        String expectedToken) {
+      return new MockDccStorageHandler(result, expectedToken, INTERNAL_SERVER_ERROR ,-1, newLinkedHashSet());
+    }
+  }
 
 }
