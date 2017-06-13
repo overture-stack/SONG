2f1678de13659367853a83f20024c56d4391d8c7
rtisma <rtisma@gmail.com>
2017-06-13 08:21:50 -0400

    updated profile for tests to dev , secure

--- song-server/src/test/java/org/icgc/dcc/song/server/service/AnalysisServiceTest.java
+++ song-server/src/test/java/org/icgc/dcc/song/server/service/AnalysisServiceTest.java
@@ -18,11 +18,11 @@
  */
 package org.icgc.dcc.song.server.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.io.InputStream;
-import java.util.ArrayList;
-
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import lombok.SneakyThrows;
+import lombok.val;
 import org.assertj.core.api.Assertions;
 import org.flywaydb.test.annotation.FlywayTest;
 import org.flywaydb.test.junit.FlywayTestExecutionListener;
@@ -37,18 +37,16 @@ import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import java.io.InputStream;
+import java.util.ArrayList;
 
-import lombok.SneakyThrows;
-import lombok.val;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, FlywayTestExecutionListener.class })
 @FlywayTest
-@ActiveProfiles("dev")
+@ActiveProfiles({"dev", "secure"})
 public class AnalysisServiceTest {
 
   @Autowired
