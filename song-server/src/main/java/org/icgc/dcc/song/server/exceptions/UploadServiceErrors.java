507509a65e3df86679fba994cdf1eefbb16fbfbd
rtisma <rtisma@gmail.com>
2017-06-19 12:25:46 -0400

    updated

--- song-server/src/main/java/org/icgc/dcc/song/server/exceptions/UploadServiceErrors.java
+++ song-server/src/main/java/org/icgc/dcc/song/server/exceptions/UploadServiceErrors.java
@@ -1,8 +1,21 @@
 package org.icgc.dcc.song.server.exceptions;
 
-/**
- * Created by rtisma on 2017-06-16.
- */
-public class UploadServiceErrors {
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
+
+@Getter
+@RequiredArgsConstructor
+public enum UploadServiceErrors implements ServerError {
+
+  UPLOAD_REPOSITORY_CREATE_RECORD("upload.repository.create.record", INTERNAL_SERVER_ERROR),
+  PAYLOAD_PARSING("payload.parsing", UNPROCESSABLE_ENTITY);
+
+  @NonNull private final String errorId;
+  @NonNull private final HttpStatus httpStatus;
 
 }
