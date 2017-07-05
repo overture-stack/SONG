c9636de32cfe83118768916eccd33dfe8353213e
rtisma <rtisma@gmail.com>
2017-07-05 17:35:54 -0400

    updated

--- song-server/src/main/java/org/icgc/dcc/song/server/importer/model/PortalSampleData.java
+++ song-server/src/main/java/org/icgc/dcc/song/server/importer/model/PortalSampleData.java
@@ -4,6 +4,8 @@ import lombok.Builder;
 import lombok.NonNull;
 import lombok.Value;
 
+import static java.util.Objects.isNull;
+
 @Value
 @Builder
 public class PortalSampleData {
@@ -11,6 +13,11 @@ public class PortalSampleData {
   @NonNull private final String id;
   @NonNull private final String analyzedId;
   @NonNull private final String study;
-  private final String libraryStrategy;
+  private final String libraryStrategy; //not always defined
+
+  public boolean isLibraryStrategyNull(){
+    return isNull(libraryStrategy);
+  }
+
 
 }
