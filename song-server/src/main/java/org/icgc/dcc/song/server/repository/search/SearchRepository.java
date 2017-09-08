4788a382dee196f0995d917fe9c1239999dd1e6c
rtisma <rtisma@gmail.com>
2017-09-08 11:41:12 -0400

    added new files

---
+++ song-server/src/main/java/org/icgc/dcc/song/server/repository/search/SearchRepository.java
@@ -0,0 +1,29 @@
+package org.icgc.dcc.song.server.repository.search;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.icgc.dcc.song.server.repository.mapper.InfoSearchResponseMapper;
+import org.skife.jdbi.v2.Handle;
+
+import java.util.List;
+
+import static org.icgc.dcc.song.server.repository.search.SearchQueryBuilder.createSearchQueryBuilder;
+
+@RequiredArgsConstructor
+public class SearchRepository {
+
+  private final Handle handle;
+  private final InfoSearchResponseMapper infoSearchResponseMapper;
+
+  public List<InfoSearchResponse> infoSearch(boolean includeInfo, @NonNull List<SearchTerm> searchTerms){
+    val searchQueryBuilder = createSearchQueryBuilder(includeInfo);
+    searchTerms.forEach(searchQueryBuilder::add);
+    val query = searchQueryBuilder.build();
+    val r = handle.createQuery(query);
+    val q = r.map(infoSearchResponseMapper);
+    val out = q.list();
+    return out;
+  }
+
+}
