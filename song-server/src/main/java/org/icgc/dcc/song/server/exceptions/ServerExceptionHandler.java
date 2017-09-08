4378cdd99bd6692ad032bfcd0effa6014c18b7bd
rtisma <rtisma@gmail.com>
2017-09-08 10:43:38 -0400

    updated return response

--- song-server/src/main/java/org/icgc/dcc/song/server/exceptions/ServerExceptionHandler.java
+++ song-server/src/main/java/org/icgc/dcc/song/server/exceptions/ServerExceptionHandler.java
@@ -1,5 +1,7 @@
 package org.icgc.dcc.song.server.exceptions;
 
+import com.google.common.base.Throwables;
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.icgc.dcc.song.core.exceptions.ServerException;
 import org.icgc.dcc.song.core.exceptions.SongError;
@@ -9,9 +11,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
+import static com.google.common.base.Throwables.getStackTraceAsString;
+import static java.lang.String.format;
+import static org.icgc.dcc.common.core.util.Splitters.NEWLINE;
+import static org.icgc.dcc.common.core.util.stream.Collectors.toImmutableList;
 import static org.icgc.dcc.song.core.exceptions.ServerErrors.UNKNOWN_ERROR;
 
+@Slf4j
 @ControllerAdvice
 public class ServerExceptionHandler {
 
@@ -32,8 +40,18 @@ public class ServerExceptionHandler {
     error.setHttpStatus(UNKNOWN_ERROR.getHttpStatus());
     error.setErrorId(UNKNOWN_ERROR.getErrorId());
     error.setMessage(ex.getMessage());
-    error.setStackTraceElementArray(ex.getStackTrace());
+    val rootCause = Throwables.getRootCause(ex);
+    error.setDebugMessage(format("[ROOT_CAUSE] -> %s: %s", rootCause.getClass().getName(), rootCause.getMessage()));
+    error.setStackTrace(getFullStackTraceList(ex));
+    log.error(error.toPrettyJson());
     return error.getResponseEntity();
+  }
+
+  private static List<String> getFullStackTraceList(Throwable t){
+    return NEWLINE.splitToList(getStackTraceAsString(t))
+        .stream()
+        .map(String::trim)
+        .collect(toImmutableList());
   }
 
 
