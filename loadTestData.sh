14df0735388f6700c043ccece027156b1b96628e
rtisma <rtisma@gmail.com>
2017-09-12 16:22:21 -0400

    updated

--- loadTestData.sh
+++ loadTestData.sh
@@ -1,7 +1,5 @@
-SCRIPT_MAC=`readlink -f ${BASH_SOURCE[0]}`
-SCRIPT_MAC_DIR=$( dirname  "${SCRIPT_MAC}")
-SING_EXE="$SCRIPT_MAC_DIR/sing"
-ROOT_LOC="$SCRIPT_MAC_DIR/song-server/src/test/resources/documents/search"
+SING_EXE="./sing"
+ROOT_LOC="song-server/src/test/resources/documents/search"
 $SING_EXE upload  -f "$ROOT_LOC/testData_0.json" | cut -d '"' -f 8 | xargs ./sing save -u
 $SING_EXE upload  -f "$ROOT_LOC/testData_1.json" | cut -d '"' -f 8 | xargs ./sing save -u
 $SING_EXE upload  -f "$ROOT_LOC/testData_2.json" | cut -d '"' -f 8 | xargs ./sing save -u
