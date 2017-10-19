6575c8480a74011312b3ecb053af797792f9025b
rtisma <rtisma@gmail.com>
2017-10-18 20:47:43 -0400

    updated auth

--- docker/auth/init/run.sh
+++ docker/auth/init/run.sh
@@ -7,7 +7,7 @@ SIMPLE_AUTH_DB=$SIMPLE_AUTH_ROOT/db.sqlite3
 echo "INIT_DB=$INIT_DB"
 echo "DATA_DB=$DATA_DB"
 echo "SIMPLE_AUTH_DB=$SIMPLE_AUTH_DB"
-echo "SERVER_PORT=$SERVER_PORT"
+echo "AUTH_PORT=$AUTH_PORT"
 
 ## Initially, copy the init db if nothing already exists in the auth_data volume
 if [ ! -f $DATA_DB ]; then
@@ -19,5 +19,5 @@ rm -f $SIMPLE_AUTH_DB
 ln -s $DATA_DB $SIMPLE_AUTH_DB
 
 ## Run the server
-python $SIMPLE_AUTH_ROOT/manage.py runserver  0.0.0.0:$SERVER_PORT ; FOR_100_YEARS=$((100*365*24*60*60));while true;do sleep $FOR_100_YEARS;done
+python $SIMPLE_AUTH_ROOT/manage.py runserver  0.0.0.0:$AUTH_PORT ; FOR_100_YEARS=$((100*365*24*60*60));while true;do sleep $FOR_100_YEARS;done
 
