65add5b2c28efbf05b07107922c512800625e55f
rtisma <rtisma@gmail.com>
2017-10-25 15:34:52 +0000

    added option to define mangement and jmx ports

--- docker/id/scripts/run.sh
+++ docker/id/scripts/run.sh
@@ -1,5 +1,8 @@
 echo "Starting ID Server..."
 echo '$ID_HOME='$ID_HOME
+echo '$ID_PORT='$ID_PORT
+echo '$ID_JMX_PORT='$ID_JMX_PORT
+echo '$ID_MANAGEMENT_PORT='$ID_JMX_PORT
 echo '$POSTGRES_HOST='$POSTGRES_HOST
 echo '$POSTGRES_PORT='$POSTGRES_PORT
 echo '$POSTGRES_DB='$POSTGRES_DB
@@ -13,6 +16,9 @@ echo '$AUTH_SERVER_CLIENTSECRET='$AUTH_SERVER_CLIENTSECRET
 
 
 $ID_HOME/bin/dcc-id-server start \
+	wrapper.java.additional.2=-Dcom.sun.management.jmxremote.port=${ID_JMX_PORT} \
+	wrapper.app.parameter.6=--server.port=${ID_PORT} \
+	wrapper.app.parameter.7=--management.port=${ID_MANAGEMENT_PORT} \
 	set.SPRING_DATASOURCE_URL="jdbc:postgresql://${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}" \
 	set.SPRING_DATASOURCE_USERNAME=$POSTGRES_USER \
 	set.SPRING_DATASOURCE_PASSWORD=$POSTGRES_PASSWORD \
