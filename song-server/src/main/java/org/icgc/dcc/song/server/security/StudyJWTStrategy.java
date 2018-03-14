0296de00621597cb82228068e8924eccb7b48f43
Alex <lepsalex@gmail.com>
2018-03-14 15:03:57 -0400

    JWTStrategy tests passing

--- song-server/src/main/java/org/icgc/dcc/song/server/security/StudyJWTStrategy.java
+++ song-server/src/main/java/org/icgc/dcc/song/server/security/StudyJWTStrategy.java
@@ -4,15 +4,27 @@ import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.icgc.dcc.song.server.jwt.JWTUser;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+import static org.icgc.dcc.common.core.util.Joiners.DOT;
+
 @Slf4j
 @Component
 @Profile("jwt")
 public class StudyJWTStrategy implements StudyStrategyInterface {
+
+    @Value("${auth.server.prefix}")
+    protected String scopePrefix;
+
+    @Value("${auth.server.suffix}")
+    protected String scopeSuffix;
 
     public boolean authorize(@NonNull Authentication authentication, @NonNull final String studyId) {
         log.info("Checking authorization with study id {}", studyId);
@@ -20,8 +32,26 @@ public class StudyJWTStrategy implements StudyStrategyInterface {
         val details = (OAuth2AuthenticationDetails) authentication.getDetails();
         val user = (JWTUser) details.getDecodedDetails();
 
-        final String REQUIRED_ROLE = "song.STUDY001.upload";
-        return user.getRoles().contains(REQUIRED_ROLE);
+        return verify(user, studyId);
     }
 
+    boolean verify(JWTUser user, String studyId) {
+        final val roles = user.getRoles();
+        val check = roles.stream().filter(s -> isGranted(s, studyId)).collect(toList());
+        return !check.isEmpty();
+    }
+
+    private boolean isGranted(String tokenScope, String studyId) {
+        log.info("Checking JWT's scope '{}', server's scopePrefix='{}', studyId '{}', scopeSuffix='{}'",
+                tokenScope, scopePrefix, studyId, scopeSuffix);
+        return getSystemScope().equals(tokenScope) || getEndUserScope(studyId).equals(tokenScope); //short-circuit
+    }
+
+    private String getEndUserScope(String studyId) {
+        return DOT.join(scopePrefix, studyId.toUpperCase(), scopeSuffix);
+    }
+
+    private String getSystemScope() {
+        return DOT.join(scopePrefix, scopeSuffix);
+    }
 }
--- song-server/src/test/java/org/icgc/dcc/song/server/security/StudyJWTStrategyTest.java
+++ song-server/src/test/java/org/icgc/dcc/song/server/security/StudyJWTStrategyTest.java
@@ -8,13 +8,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.authentication.TestingAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
-import org.springframework.security.oauth2.provider.OAuth2Request;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -33,9 +26,6 @@ public class StudyJWTStrategyTest {
 
     @Autowired
     StudyJWTStrategy studyJWTStrategy;
-
-    private String clientId = "client";
-    private String studyId = "study001";
 
     private JWTUser unapprovedUser;
     private JWTUser approvedWithAccessUser;
@@ -59,38 +49,21 @@ public class StudyJWTStrategyTest {
         approvedWithAccessUser = userBuilder.build();
         approvedWithoutAccessUser = userBuilder.build();
 
+        // Careful with underlying references
         unapprovedUser.setStatus("Not Approved");
         approvedWithAccessUser.setRoles(Arrays.asList("song.STUDY001.upload"));
         approvedWithoutAccessUser.setRoles(Arrays.asList("song.STUDY002.upload"));
     }
 
     @Test
-    public void testGoodOAuth2Authentication() throws Exception {
-        val auth = getJWTOAuth2Authentication(approvedWithAccessUser);
-        val isVerified = studyJWTStrategy.authorize(auth, studyId);
+    public void testJWTUserWithAccess() throws Exception {
+        val isVerified = studyJWTStrategy.verify(approvedWithAccessUser, "STUDY001");
         assertThat(isVerified).isTrue();
     }
 
     @Test
-    public void testBadOAuth2Authentication() throws Exception {
-        val auth = getJWTOAuth2Authentication(approvedWithoutAccessUser);
-        val isVerified = studyJWTStrategy.authorize(auth, studyId);
+    public void testJWTUserWithoutAccess() throws Exception {
+        val isVerified = studyJWTStrategy.verify(approvedWithoutAccessUser, "STUDY001");
         assertThat(isVerified).isFalse();
-    }
-
-    private OAuth2Authentication getJWTOAuth2Authentication(JWTUser jwtUser) {
-        OAuth2Request oauth2Request = new OAuth2Request(null, clientId, null, true, null, null, null, null, null);
-        Authentication userauth = getAuthentication(jwtUser);
-        OAuth2Authentication oauth2auth = new OAuth2Authentication(oauth2Request, userauth);
-        return oauth2auth;
-    }
-
-    private Authentication getAuthentication(JWTUser jwtUser) {
-        List<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList();
-        User user = new User("user", "", true, true, true, true, authorities);
-        TestingAuthenticationToken token = new TestingAuthenticationToken(user, null, authorities);
-        token.setAuthenticated(true);
-        token.setDetails(jwtUser);
-        return token;
     }
 }
