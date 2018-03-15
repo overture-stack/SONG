96b1194e37a4b9f16fed9749e3395da84f4dc10c
rtisma <rtisma@gmail.com>
2018-03-15 08:40:20 -0400

    feat(FileService): Better error handling for FileService
    BREAKING CHANGE: The following FileService methods can now throw a STUDY_ID_DOES_NOT_EXIST
    exception: create, save
    
     The following FileService methods can now throw an ANALYSIS_ID_NOT_FOUND
    exception: create, save
    
     The following FileService methods can now throw a FILE_NOT_FOUND
    exception: read, update, delete

--- song-server/src/main/java/org/icgc/dcc/song/server/service/FileService.java
+++ song-server/src/main/java/org/icgc/dcc/song/server/service/FileService.java
@@ -22,70 +22,111 @@ import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.val;
 import org.icgc.dcc.song.server.model.entity.File;
+import org.icgc.dcc.song.server.repository.AnalysisRepository;
 import org.icgc.dcc.song.server.repository.FileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import static java.util.Objects.isNull;
-import static org.icgc.dcc.song.core.exceptions.ServerErrors.FILE_RECORD_FAILED;
+import static org.icgc.dcc.song.core.exceptions.ServerErrors.FILE_NOT_FOUND;
+import static org.icgc.dcc.song.core.exceptions.ServerErrors.FILE_REPOSITORY_CREATE_RECORD;
+import static org.icgc.dcc.song.core.exceptions.ServerErrors.FILE_REPOSITORY_DELETE_RECORD;
+import static org.icgc.dcc.song.core.exceptions.ServerErrors.FILE_REPOSITORY_UPDATE_RECORD;
 import static org.icgc.dcc.song.core.exceptions.ServerException.checkServer;
 import static org.icgc.dcc.song.core.utils.Responses.OK;
+import static org.icgc.dcc.song.server.service.AnalysisService.checkAnalysis;
 
 @Service
 @NoArgsConstructor
 public class FileService {
 
   @Autowired
+  AnalysisRepository analysisRepository;
+  @Autowired
   FileRepository repository;
   @Autowired
   FileInfoService infoService;
   @Autowired
   IdService idService;
+  @Autowired
+  StudyService studyService;
 
   public String create(@NonNull String analysisId, @NonNull String studyId, @NonNull File file) {
+    checkAnalysisAndStudyId(analysisId, studyId);
+
     val id = idService.generateFileId(analysisId, file.getFileName());
     file.setObjectId(id);
     file.setStudyId(studyId);
     file.setAnalysisId(analysisId);
 
     val status = repository.create(file);
-
     checkServer(status == 1,
-        getClass(), FILE_RECORD_FAILED, "Cannot create File: %s", file.toString());
+        getClass(), FILE_REPOSITORY_CREATE_RECORD,
+    "Could not create File record for file with objectId '%s': %s",
+        file.getObjectId(), file);
     infoService.create(id, file.getInfoAsString());
 
     return id;
   }
 
+  public boolean isFileExist(@NonNull String id){
+    return !isNull(repository.read(id));
+  }
+
+  public void checkFileExists(String id){
+    fileNotFoundCheck(isFileExist(id), id);
+  }
+
+  public void checkFileExists(@NonNull File file){
+    checkFileExists(file.getObjectId());
+  }
+
   public File read(@NonNull String id) {
     val f = repository.read(id);
-    if (f==null) {
-      return null;
-    }
+    fileNotFoundCheck(!isNull(f), id);
     f.setInfo(infoService.readNullableInfo(id));
     return f;
   }
 
   public String update(@NonNull File f) {
-    repository.update(f);
+    checkFileExists(f);
+    val status = repository.update(f);
+    checkServer(status == 1, getClass(), FILE_REPOSITORY_UPDATE_RECORD,
+        "Cannot update objectId '%s' for file '%s'",
+    f.getObjectId(), f);
     infoService.update(f.getObjectId(), f.getInfoAsString());
     return OK;
   }
 
   public String delete(@NonNull String id) {
-    repository.delete(id);
+    checkFileExists(id);
+    val status = repository.delete(id);
+    checkServer(status == 1, getClass(), FILE_REPOSITORY_DELETE_RECORD,
+        "Cannot delete file with objectId '%s'", id);
     infoService.delete(id);
     return OK;
   }
 
   public String save(@NonNull String analysisId, @NonNull String studyId, @NonNull File file) {
+    checkAnalysisAndStudyId(analysisId, studyId);
     String fileId = repository.findByBusinessKey(analysisId, file.getFileName());
     if (isNull(fileId)) {
       fileId = create(analysisId, studyId, file);
     } else {
-      repository.update(file);
+      file.setObjectId(fileId);
+      update(file);
     }
     return fileId;
+  }
+
+  private static void fileNotFoundCheck(boolean expression, @NonNull String id){
+    checkServer(expression, FileService.class.getClass(), FILE_NOT_FOUND,
+        "The File with objectId '%s' does not exist", id);
+  }
+
+  private void checkAnalysisAndStudyId(@NonNull String analysisId, @NonNull String studyId){
+    checkAnalysis(analysisRepository, analysisId);
+    studyService.checkStudyExist(studyId);
   }
 
 }
