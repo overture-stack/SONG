6687093d2bbcd0811bc140c8f4cc61a14b307c96
rtisma <rtisma@gmail.com>
2018-03-26 00:35:25 -0400

    test: Added ExportService test

--- song-server/src/test/java/org/icgc/dcc/song/server/service/ExportServiceTest.java
+++ song-server/src/test/java/org/icgc/dcc/song/server/service/ExportServiceTest.java
@@ -1,9 +1,17 @@
 package org.icgc.dcc.song.server.service;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.icgc.dcc.song.core.utils.JsonUtils;
+import org.icgc.dcc.song.core.utils.RandomGenerator;
+import org.icgc.dcc.song.server.model.analysis.SequencingReadAnalysis;
+import org.icgc.dcc.song.server.model.entity.Donor;
+import org.icgc.dcc.song.server.model.entity.File;
+import org.icgc.dcc.song.server.model.entity.composites.CompositeEntity;
+import org.icgc.dcc.song.server.model.enums.UploadStates;
+import org.icgc.dcc.song.server.repository.AnalysisRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +22,19 @@ import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.icgc.dcc.song.core.utils.RandomGenerator.createRandomGenerator;
+import static org.icgc.dcc.song.server.model.enums.UploadStates.resolveState;
+import static org.icgc.dcc.song.server.utils.AnalysisGenerator.createAnalysisGenerator;
+import static org.icgc.dcc.song.server.utils.TestConstants.DEFAULT_STUDY_ID;
+
 @Slf4j
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @TestExecutionListeners({ DependencyInjectionTestExecutionListener.class})
 @ActiveProfiles("dev")
 public class ExportServiceTest {
+  private static final String ANALYSIS_ID = "analysisId";
 
   @Autowired
   private ExportService exportService;
@@ -30,34 +45,74 @@ public class ExportServiceTest {
   @Autowired
   private AnalysisService analysisService;
 
+  @Autowired private AnalysisRepository analysisRepository;
+  @Autowired private AnalysisInfoService analysisInfoService;
+  @Autowired private SequencingReadInfoService sequencingReadInfoService;
+  @Autowired private DonorService donorService;
+  @Autowired private FileService fileService;
+
+  private final RandomGenerator randomGenerator = createRandomGenerator(ExportServiceTest.class.getSimpleName());
+
+  /**
+   * Delete the analysis
+   */
+  private void deleteAnalysis(SequencingReadAnalysis a){
+    analysisRepository.deleteSequencingRead(a.getAnalysisId());
+    analysisRepository.deleteCompositeEntities(a.getAnalysisId());
+    analysisInfoService.delete(a.getAnalysisId());
+    sequencingReadInfoService.delete(a.getAnalysisId());
+    a.getSample().stream()
+        .map(CompositeEntity::getDonor)
+        .map(Donor::getDonorId)
+        .forEach(x -> donorService.delete(a.getStudy(), x));
+    a.getFile().stream()
+        .map(File::getObjectId)
+        .forEach(x -> fileService.delete(x));
+    analysisRepository.deleteAnalysis(a.getAnalysisId());
+  }
 
   @Test
   @SneakyThrows
   public void testFullLoop(){
-    /*
-    val generator = Generators.randomBasedGenerator();
+    // Generate an analysis and create it (backdoor creation)
+    val analysisGenerator = createAnalysisGenerator(DEFAULT_STUDY_ID, analysisService, randomGenerator);
+    val expectedSeqAnalysis = analysisGenerator.createDefaultRandomSequencingReadAnalysis();
 
-    val analysisPayload = new SequencingReadAnalysis();
-    analysisPayload.setStudy("ABC123");
-    val f = File.create(null, null, "myFile.bam", "AB");
+    // Export the analysis
+    val json =  exportService.exportPayload(expectedSeqAnalysis.getAnalysisId(), false);
 
-    val json = exportService.exportPayload("AN2", true);
-    ((ObjectNode)json).put("analysisId", "AN_ROB_1");
+    // Delete the previously created analysis so it can be created using the uploadService (frontdoor creation)
+    deleteAnalysis(expectedSeqAnalysis);
+    ((ObjectNode)json).put(ANALYSIS_ID, expectedSeqAnalysis.getAnalysisId());
 
+    // Upload and check if successful
     val payload = JsonUtils.toJson(json);
-    val uploadStatus = uploadService.upload("ABC123", payload, false);
+    val uploadStatus = uploadService.upload(DEFAULT_STUDY_ID, payload, false);
     val status1 = fromStatus(uploadStatus, "status");
     assertThat(status1).isEqualTo("ok");
     val uploadId = fromStatus(uploadStatus, "uploadId");
+
+    // Check Status and check if validated
     val statusResponse = uploadService.read(uploadId);
     val uploadState = resolveState(statusResponse.getState());
     assertThat(uploadState).isEqualTo(UploadStates.VALIDATED);
-    val analysisResponse = uploadService.save("ABC123", uploadId, false);
+
+    // Save and check if successful
+    val analysisResponse = uploadService.save(DEFAULT_STUDY_ID, uploadId, true);
     val status2 = fromStatus(analysisResponse, "status");
-    val analysisId = fromStatus(analysisResponse, "analysisId");
-    val analysis = analysisService.read(analysisId);
-    log.info("sdf");
-    */
+    assertThat(status2).isEqualTo("ok");
+    val analysisId = fromStatus(analysisResponse, ANALYSIS_ID);
+
+    // Get analysis and compare against expected
+    val actualAnalysis = SequencingReadAnalysis.class.cast(analysisService.read(analysisId));
+    assertThat(actualAnalysis.getExperiment()).isEqualToComparingFieldByField(expectedSeqAnalysis.getExperiment());
+    assertThat(actualAnalysis.getAnalysisType()).isEqualTo(expectedSeqAnalysis.getAnalysisType());
+    assertThat(actualAnalysis.getAnalysisId()).isEqualTo(expectedSeqAnalysis.getAnalysisId());
+    assertThat(actualAnalysis.getAnalysisState()).isEqualTo(expectedSeqAnalysis.getAnalysisState());
+    assertThat(actualAnalysis.getInfo()).isEqualTo(expectedSeqAnalysis.getInfo());
+    assertThat(actualAnalysis.getStudy()).isEqualTo(expectedSeqAnalysis.getStudy());
+    assertThat(actualAnalysis.getFile()).containsAll(expectedSeqAnalysis.getFile());
+    assertThat(actualAnalysis.getSample()).containsAll(expectedSeqAnalysis.getSample());
   }
 
   @SneakyThrows
