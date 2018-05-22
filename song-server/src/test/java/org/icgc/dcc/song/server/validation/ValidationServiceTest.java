687db5b76e4aacb4076003797059f675b2190d35
rtisma <rtisma@gmail.com>
2018-05-22 10:35:09 -0400

    test: Updated tests for fileMd5sum and analysisId validation

--- song-server/src/test/java/org/icgc/dcc/song/server/validation/ValidationServiceTest.java
+++ song-server/src/test/java/org/icgc/dcc/song/server/validation/ValidationServiceTest.java
@@ -18,7 +18,9 @@ package org.icgc.dcc.song.server.validation;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.collect.Maps;
 import lombok.val;
+import org.icgc.dcc.song.core.utils.RandomGenerator;
 import org.icgc.dcc.song.server.service.ValidationService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -29,7 +31,12 @@ import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.util.Lists.newArrayList;
+import static org.icgc.dcc.common.core.util.Splitters.PIPE;
+import static org.icgc.dcc.song.core.utils.RandomGenerator.createRandomGenerator;
 import static org.icgc.dcc.song.server.utils.TestFiles.getJsonNodeFromClasspath;
 
 @SpringBootTest
@@ -111,6 +118,108 @@ public class ValidationServiceTest {
     val results=service.validate(payload, SEQ_READ);
     assertThat(results).isNotEmpty();
     assertThat(results.get().contains("Invalid JSON document submitted:")).isTrue();
+  }
+
+  @Test
+  public void testFileMd5Validation(){
+    val testMap = Maps.<String, String>newHashMap();
+    testMap.put(SEQ_READ, "sequencingRead.json");
+    testMap.put(VAR_CALL, "variantCall.json");
+
+    for (val testDataEntry : testMap.entrySet() ){
+      val schemaType = testDataEntry.getKey();
+      val testFileName = testDataEntry.getValue();
+
+      val payload = getJsonFile(testFileName);
+      val fileNodes = newArrayList(payload.path("file"));
+      assertThat(fileNodes.size()).isGreaterThan(1);
+      val fileNode0 = ((ObjectNode)fileNodes.get(0)).put("fileMd5sum", "q123"); // less than 32 and non-hex number
+      val fileNode1 = ((ObjectNode)fileNodes.get(1)).put("fileMd5sum", "q0123456789012345678901234567890123456789"); //more than 32 and non-hex number
+
+      val results = service.validate(payload.toString(), schemaType);
+
+      assertThat(results).isNotEmpty();
+
+      val errors = PIPE.splitToList(results.get());
+      assertThat(errors).hasSize(2);
+      for (val error : errors){
+        assertThat(error).contains("fileMd5sum: does not match the regex pattern");
+      }
+    }
+
+  }
+  @Test
+  public void testFileMd5sumValidation(){
+    val md5 = randomGenerator.generateRandomMD5();
+    assertThat(md5.length()).isEqualTo(32);
+    for (val schemaType : DEFAULT_TEST_FILE_MAP.keySet()){
+      runFileMd5sumValidationTest(md5+"1", schemaType, true); // invalidate >32 chars
+      runFileMd5sumValidationTest(md5.substring(0,31), schemaType, true); // invalidate <32 chars
+      runFileMd5sumValidationTest(md5.substring(0,31)+"q", schemaType, true); //invalidate non-hex value
+      runFileMd5sumValidationTest(md5, schemaType, false); //validate hex value with 32 chars
+    }
+  }
+
+  @Test
+  public void testAnalysisIdValidation(){
+    val array = new String[]{"_", "-"};
+    for (val schemaType : DEFAULT_TEST_FILE_MAP.keySet()){
+      runAnalysisIdValidationErrorTest(randomGenerator.generateRandomAsciiString(37), schemaType, true); // invalidate >36 chars
+      for (val c : array){
+        runAnalysisIdValidationErrorTest(c+randomGenerator.generateRandomAsciiString(35), schemaType, true); //invalidate char at beginning
+        runAnalysisIdValidationErrorTest(c+randomGenerator.generateRandomAsciiString(36), schemaType, true); // invalidate >36 and char at begining
+        runAnalysisIdValidationErrorTest(randomGenerator.generateRandomAsciiString(35)+c, schemaType, true); //invalidate char at end
+        runAnalysisIdValidationErrorTest(randomGenerator.generateRandomAsciiString(36)+c, schemaType, true); // invalidate >36 and char at end
+      }
+      runAnalysisIdValidationErrorTest(randomGenerator.generateRandomAsciiString(1), schemaType, true);
+      runAnalysisIdValidationErrorTest(randomGenerator.generateRandomAsciiString(2), schemaType, true);
+      runAnalysisIdValidationErrorTest(randomGenerator.generateRandomAsciiString(3), schemaType, false);
+      runAnalysisIdValidationErrorTest(randomGenerator.generateRandomAsciiString(36), schemaType, false);
+    }
+  }
+
+  private final RandomGenerator randomGenerator = createRandomGenerator(ValidationServiceTest.class.getSimpleName());
+
+  private static final Map<String, String> DEFAULT_TEST_FILE_MAP = Maps.newHashMap();
+  static {
+    DEFAULT_TEST_FILE_MAP.put(SEQ_READ, "sequencingRead.json");
+    DEFAULT_TEST_FILE_MAP.put(VAR_CALL, "variantCall.json");
+  }
+
+  private void runAnalysisIdValidationErrorTest(String analysisId, String schemaType, boolean shouldBeError){
+    val testFileName = DEFAULT_TEST_FILE_MAP.get(schemaType);
+
+    val payload = getJsonFile(testFileName);
+    ((ObjectNode)payload).put("analysisId", analysisId);
+
+    val results = service.validate(payload.toString(), schemaType);
+
+    if (shouldBeError){
+      assertThat(results).isNotEmpty();
+      assertThat(results.get()).contains("analysisId: does not match the regex pattern");
+    } else {
+      assertThat(results).as("Expecting validation not to have an error").isEmpty();
+    }
+  }
+
+  private void runFileMd5sumValidationTest(String md5, String schemaType, boolean shouldBeError){
+    val testFileName = DEFAULT_TEST_FILE_MAP.get(schemaType);
+
+    val payload = getJsonFile(testFileName);
+    val fileNodes = newArrayList(payload.path("file"));
+    assertThat(fileNodes).isNotEmpty();
+    for (val fileNode : fileNodes){
+      ((ObjectNode)fileNode).put("fileMd5sum", md5);
+    }
+
+    val results = service.validate(payload.toString(), schemaType);
+
+    if (shouldBeError){
+      assertThat(results).isNotEmpty();
+      assertThat(results.get()).contains("fileMd5sum: does not match the regex pattern");
+    } else {
+      assertThat(results).as("Expecting validation not to have an error: %s", results.orElse(null)).isEmpty();
+    }
   }
 
   private JsonNode getJsonFile(String name) {
