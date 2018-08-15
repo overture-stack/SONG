b0e4329616ec8e39d73a0bd07c159db9ec1965fe
khartmann <kevin.hartmann@oicr.on.ca>
2018-08-15 16:17:31 -0400

    Bugfix: Only start postgres is it isn't already running

--- build.sh
+++ build.sh
@@ -10,17 +10,16 @@ if ! type "$PROG" &> /dev/null; then
     exit 1
 else
     echo "SUCCESS - Postgresql is installed"
-    status=$(echo "select 1" | $PROG $DB) 
+    echo "select 1" | $PROG $DB &> /dev/null 
 
     if [ $? != 0 ]; then
        echo "Postgres is not running..."
-    fi
-
-    if [ -z "$PGDATA" ];then 
-       initdb $DB 
-       pg_ctl start -D $DB 
-    else 
-       pg_ctl start 
+       if [ -z "$PGDATA" ];then 
+          initdb $DB 
+          pg_ctl start -D $DB 
+       else 
+          pg_ctl start 
+       fi
     fi
     echo "create database $DB;" | psql postgres 
     mvn clean package
