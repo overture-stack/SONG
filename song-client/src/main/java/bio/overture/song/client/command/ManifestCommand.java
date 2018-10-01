2d2982fc92c263b354d95450d8d8e910126dd830
rtisma <rtisma@gmail.com>
2018-10-01 10:18:08 -0400

    feat(song-client): Takes input dir and resolves real abs path in manifest

--- song-client/src/main/java/bio/overture/song/client/command/ManifestCommand.java
+++ song-client/src/main/java/bio/overture/song/client/command/ManifestCommand.java
@@ -25,15 +25,22 @@ import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Joiner;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static java.nio.file.Files.exists;
+import static java.nio.file.Files.isDirectory;
+import static java.nio.file.Paths.get;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.StreamSupport.stream;
 
 @RequiredArgsConstructor
 @Parameters(commandDescription = "Generate a manifest file for the analysis with the specified analysis id")
@@ -44,8 +51,12 @@ public class ManifestCommand extends Command {
   @Parameter(names = { "-a", "--analysis-id" })
   private String analysisId;
 
-  @Parameter(names = { "--file", "-f" }, description = "Filename to save file in (if not set, displays manifest on standard output")
+  @Parameter(names = { "--file", "-f" },
+      description = "Filename to save file in (if not set, displays manifest on standard output", required = true)
   private String fileName;
+
+  @Parameter(names = { "-d", "--input-dir"}, description = "Directory containing the files", required = true)
+  private String inputDirName;
 
   @NonNull
   private Registry registry;
@@ -53,11 +64,18 @@ public class ManifestCommand extends Command {
   @NonNull
   private Config config;
 
+  private Path inputDirPath;
+
   @Override
   public void run() throws IOException {
     if (analysisId == null) {
       analysisId = getJson().at("/analysisId").asText("");
     }
+
+    inputDirPath = get(inputDirName);
+
+    checkArgument(exists(inputDirPath), "The input path '%s' does not exist", inputDirName);
+    checkArgument(isDirectory(inputDirPath), "The input path '%s' is not a directory", inputDirName);
 
     val status = registry.getAnalysisFiles(config.getStudyId(), analysisId);
 
@@ -67,13 +85,20 @@ public class ManifestCommand extends Command {
     }
 
     val m = createManifest(analysisId, status.getOutputs());
+    val missingFiles = m.getEntries().stream()
+        .map(ManifestEntry::getFileName)
+        .map(Paths::get)
+        .filter(x -> !Files.exists(x))
+        .collect(toList());
 
     if(m.getEntries().size() == 0){
       err("[SONG_CLIENT_ERROR]: the analysisId '%s' returned 0 files", analysisId);
     } else if (fileName == null) {
       output(m.toString());
+    } else if (missingFiles.size() > 0){
+      err("[SONG_CLIENT_ERROR]: The following files do not exist: \n'%s'", Joiner.on("',\n'").join(missingFiles));
     } else {
-      Files.write(Paths.get(fileName), m.toString().getBytes());
+      Files.write(get(fileName), m.toString().getBytes());
       output("Wrote manifest file '%s' for analysisId '%s'", fileName, analysisId);
     }
   }
@@ -85,9 +110,9 @@ public class ManifestCommand extends Command {
     val m = new Manifest(analysisId);
 
     Iterable<JsonNode> iter = () -> root.at(JSON_PATH_TO_FILES).iterator();
-    m.addAll(StreamSupport.stream(iter.spliterator(), false)
+    m.addAll(stream(iter.spliterator(), false)
         .map(this::jsonNodeToManifestEntry)
-        .collect(Collectors.toList()));
+        .collect(toList()));
     return m;
   }
 
@@ -97,7 +122,8 @@ public class ManifestCommand extends Command {
     val fileName = j.get("fileName");
     val fileMd5 = j.get("fileMd5sum");
 
-    return new ManifestEntry(fileId, fileName, fileMd5);
+    val path = inputDirPath.resolve(fileName);
+    return new ManifestEntry(fileId, path.toAbsolutePath().normalize().toString(), fileMd5);
   }
 
 }
