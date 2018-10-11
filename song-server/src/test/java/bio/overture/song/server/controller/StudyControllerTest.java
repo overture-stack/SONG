6b97198479847509fdafa689b5f95c9b683f61ec
Xu Deng <d8660091@gmail.com>
2018-10-11 13:53:43 -0400

    Fix style issues

--- song-server/src/test/java/bio/overture/song/server/controller/StudyControllerTest.java
+++ song-server/src/test/java/bio/overture/song/server/controller/StudyControllerTest.java
@@ -1,22 +1,24 @@
 package bio.overture.song.server.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import bio.overture.song.core.exceptions.ServerErrors;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@AutoConfigureMockMvc(secure=false)
-@ActiveProfiles("dev,test")
+@AutoConfigureMockMvc(secure = false)
+@ActiveProfiles({ "dev", "test" })
 public class StudyControllerTest {
 
     @Autowired
@@ -24,10 +26,9 @@ public class StudyControllerTest {
 
     @Test
     public void saveStudyShouldValidateStudyId() throws Exception {
-        this.mockMvc.perform(post("/studies/123/")
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
-                .content("{\"studyId\": \"456\"}"))
-                .andExpect(status().isConflict());
+        this.mockMvc
+                .perform(post("/studies/123/").contentType(APPLICATION_JSON).accept(APPLICATION_JSON)
+                        .content("{\"studyId\": \"456\"}"))
+                .andExpect(status().is(ServerErrors.STUDY_ID_MISMATCH.getHttpStatus().value()));
     }
 }
