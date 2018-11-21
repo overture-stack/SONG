2d912459ce835297ecba622d7c088bc00bf14f01
rtisma <rtisma@gmail.com>
2018-11-21 10:08:54 -0500

    chore: Updated requests to 2.20.0 due to vulnerbilities

--- song-python-sdk/setup.py
+++ song-python-sdk/setup.py
@@ -131,7 +131,7 @@ def run():
         # when your project is installed. For an analysis of
         # "install_requires" vs pip's requirements files see:
         # https://packaging.python.org/en/latest/requirements.html
-        install_requires=['requests>=2.18.4', 'dataclasses>=0.4'],
+        install_requires=['requests>=2.20.0', 'dataclasses>=0.4'],
 
         # List additional groups of dependencies here (e.g. development
         # dependencies). You can install these using the following syntax,
