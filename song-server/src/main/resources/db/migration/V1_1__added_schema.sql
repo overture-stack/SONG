1ff384f0e922c30a5c70b7ad67ca5398eea13b1a
Robert Tisma <942951+rtisma@users.noreply.github.com>
2019-07-22 17:30:05 +0200

    removed unused table in schema

--- song-server/src/main/resources/db/migration/V1_1__added_schema.sql
+++ song-server/src/main/resources/db/migration/V1_1__added_schema.sql
@@ -1,8 +1,3 @@
-CREATE TABLE experiment (
-    analysis_type            VARCHAR(255) PRIMARY KEY,
-    schema                   json NOT NULL
-);
-
 CREATE TABLE analysis_schema (
     id              BIGSERIAL PRIMARY KEY ,
     name            VARCHAR(225) NOT NULL,
