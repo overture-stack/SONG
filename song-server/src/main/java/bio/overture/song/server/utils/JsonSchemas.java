9b6785cd747f3a060a8e150b13c8a8c2a7fbe274
Robert Tisma <942951+rtisma@users.noreply.github.com>
2019-08-16 10:58:36 -0400

    updated

--- song-server/src/main/java/bio/overture/song/server/utils/JsonSchemas.java
+++ song-server/src/main/java/bio/overture/song/server/utils/JsonSchemas.java
@@ -1,10 +1,6 @@
 package bio.overture.song.server.utils;
 
-import static bio.overture.song.server.utils.JsonObjects.convertToJSONObject;
-import static lombok.AccessLevel.PRIVATE;
-
 import com.fasterxml.jackson.databind.JsonNode;
-import java.nio.file.Path;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.SneakyThrows;
@@ -12,6 +8,11 @@ import lombok.val;
 import org.everit.json.schema.Schema;
 import org.everit.json.schema.loader.SchemaClient;
 import org.everit.json.schema.loader.SchemaLoader;
+
+import java.nio.file.Path;
+
+import static lombok.AccessLevel.PRIVATE;
+import static bio.overture.song.server.utils.JsonObjects.convertToJSONObject;
 
 @NoArgsConstructor(access = PRIVATE)
 public class JsonSchemas {
@@ -22,6 +23,18 @@ public class JsonSchemas {
     return SchemaLoader.builder()
         .schemaClient(SchemaClient.classPathAwareClient())
         .resolutionScope("classpath://" + schemaDir.toString() + "/")
+        .schemaJson(jsonObject)
+        .draftV7Support()
+        .build()
+        .load()
+        .build();
+  }
+
+  @SneakyThrows
+  public static Schema buildSchema(JsonNode json) {
+    val jsonObject = convertToJSONObject(json);
+    return SchemaLoader.builder()
+        .schemaClient(SchemaClient.classPathAwareClient())
         .schemaJson(jsonObject)
         .draftV7Support()
         .build()
