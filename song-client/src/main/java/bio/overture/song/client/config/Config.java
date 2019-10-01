9d488615dbd452b430798a4a792d5009186871dc
Robert Tisma <942951+rtisma@users.noreply.github.com>
2019-10-01 14:19:44 -0400

    fixed type

--- song-client/src/main/java/bio/overture/song/client/config/Config.java
+++ song-client/src/main/java/bio/overture/song/client/config/Config.java
@@ -1,6 +1,7 @@
 package bio.overture.song.client.config;
 
 import bio.overture.song.sdk.config.impl.DefaultRetryConfig;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Config {
 
   @Builder.Default private CustomRestClientConfig client = new CustomRestClientConfig();
