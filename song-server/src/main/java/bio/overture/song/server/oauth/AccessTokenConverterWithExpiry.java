fca682800bc12abbb0e3ef2614e1f1d0d377118f
joneubank <joneubank@gmail.com>
2022-06-10 22:07:55 -0400

    Remove unused code and restore deprecated code used in testing

---
+++ song-server/src/main/java/bio/overture/song/server/oauth/AccessTokenConverterWithExpiry.java
@@ -0,0 +1,25 @@
+package bio.overture.song.server.oauth;
+
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
+
+import java.util.Map;
+
+import static bio.overture.song.server.oauth.ExpiringOauth2Authentication.from;
+import static bio.overture.song.server.utils.Scopes.extractExpiry;
+
+/**
+ * * RemoteTokenServices uses a postForMap call to convert the Oauth2 JSON response that we get from
+ * Ego into a Java Map from string to an unknown object type.
+ *
+ * <p>The default converter just extracts the scope field; we also want to extract the "exp" field,
+ * which holds the time to expiry for our token in seconds.
+ */
+public class AccessTokenConverterWithExpiry extends DefaultAccessTokenConverter {
+
+    @Override
+    public OAuth2Authentication extractAuthentication(Map<String, ?> map) {
+        long expiryInSeconds = extractExpiry(map);
+        return from(super.extractAuthentication(map), expiryInSeconds);
+    }
+}
--- song-server/src/test/java/bio/overture/song/server/security/TestStudySecurity.java
+++ song-server/src/test/java/bio/overture/song/server/security/TestStudySecurity.java
@@ -1,65 +1,66 @@
 package bio.overture.song.server.security;
 
-import static org.junit.Assert.assertEquals;
-
 import bio.overture.song.server.oauth.AccessTokenConverterWithExpiry;
+import lombok.val;
+import org.junit.Test;
+
 import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
-import lombok.val;
-import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class TestStudySecurity {
-  String TEST_STUDY = "TEST-CA";
+    String TEST_STUDY = "TEST-CA";
 
-  @Test
-  public void testValidStudyScopeOK() {
-    val m = new TreeMap<String, Object>();
-    m.put("exp", 10);
-    m.put("scope", Set.of("PROGRAMDATA-TEST-CA.READ"));
-    test_authorize(m, true);
-  }
+    @Test
+    public void testValidStudyScopeOK() {
+        val m = new TreeMap<String, Object>();
+        m.put("exp", 10);
+        m.put("scope", Set.of("PROGRAMDATA-TEST-CA.READ"));
+        test_authorize(m, true);
+    }
 
-  @Test
-  public void testExpiredTokenFails() {
-    val m = new TreeMap<String, Object>();
-    m.put("exp", 0);
-    m.put("scope", Set.of("PROGRAMDATA-TEST-CA.READ"));
-    test_authorize(m, false);
-  }
+    @Test
+    public void testExpiredTokenFails() {
+        val m = new TreeMap<String, Object>();
+        m.put("exp", 0);
+        m.put("scope", Set.of("PROGRAMDATA-TEST-CA.READ"));
+        test_authorize(m, false);
+    }
 
-  @Test
-  public void testUnauthorizedFails() {
-    val m = new TreeMap<String, Object>();
-    m.put("exp", 10);
-    m.put("scope", Collections.emptySet());
-    test_authorize(m, false);
-  }
+    @Test
+    public void testUnauthorizedFails() {
+        val m = new TreeMap<String, Object>();
+        m.put("exp", 10);
+        m.put("scope", Collections.emptySet());
+        test_authorize(m, false);
+    }
 
-  @Test
-  public void testValidGlobalScopeOK() {
-    val m = new TreeMap<String, Object>();
-    m.put("exp", 10);
-    m.put("scope", Set.of("DCC.READ"));
-    test_authorize(m, true);
-  }
+    @Test
+    public void testValidGlobalScopeOK() {
+        val m = new TreeMap<String, Object>();
+        m.put("exp", 10);
+        m.put("scope", Set.of("DCC.READ"));
+        test_authorize(m, true);
+    }
 
-  @Test
-  public void testValidGlobalScopeFails() {
-    val m = new TreeMap<String, Object>();
-    m.put("exp", 0);
-    m.put("scope", Set.of("DCC.READ"));
-    test_authorize(m, false);
-  }
+    @Test
+    public void testValidGlobalScopeFails() {
+        val m = new TreeMap<String, Object>();
+        m.put("exp", 0);
+        m.put("scope", Set.of("DCC.READ"));
+        test_authorize(m, false);
+    }
 
-  public void test_authorize(Map<String, ?> map, boolean expected) {
-    val prefix = "PROGRAMDATA-";
-    val suffix = ".READ";
-    val scope = "DCC.READ";
-    val studySecurity = new StudySecurity(prefix, suffix, scope);
-    val authentication = new AccessTokenConverterWithExpiry().extractAuthentication(map);
+    public void test_authorize(Map<String, ?> map, boolean expected) {
+        val prefix = "PROGRAMDATA-";
+        val suffix = ".READ";
+        val scope = "DCC.READ";
+        val studySecurity = new StudySecurity(prefix, suffix, scope);
+        val authentication = new AccessTokenConverterWithExpiry().extractAuthentication(map);
 
-    assertEquals(expected, studySecurity.authorize(authentication, TEST_STUDY));
-  }
+        assertEquals(expected, studySecurity.authorize(authentication, TEST_STUDY));
+    }
 }
