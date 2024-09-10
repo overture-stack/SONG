02e09d73bba52948073ad4fc2ed501ad0d85c920
Azher2Ali <121898125+Azher2Ali@users.noreply.github.com>
2024-09-10 10:15:31 -0400

    Updating the schemas

--- song-server/src/main/resources/db/migration/V1_20__update_legacy_analysis.sql
+++ song-server/src/main/resources/db/migration/V1_20__update_legacy_analysis.sql
@@ -1,8 +1,8 @@
 UPDATE public.analysis_schema
-SET  "schema"='{"type":"object","required":["experiment"],"properties":{"experiment":{"type":"object","required":["matchedNormalSampleSubmitterId","variantCallingTool"],"properties":{"variantCallingTool":{"type":"string"},"matchedNormalSampleSubmitterId":{"type":"string"},"fileTypes":{"enum":["VCF","BAM","OTHER_CUSTOM_TYPE"],"type":"string"}}}}}'::jsonb
-where name = 'variantCall';
+SET "schema"='{"type":"object","required":["experiment"],"properties":{"experiment":{"type":"object","required":["matchedNormalSampleSubmitterId","variantCallingTool"],"properties":{"fileTypes":{"oneOf":[{"$ref":"classpath:/schemas/analysis/analysisBase.json#/definitions/file/fileType"},{"type":"string"}]},"variantCallingTool":{"type":"string"},"matchedNormalSampleSubmitterId":{"type":"string"}}}}}'::jsonb
+WHERE "name"='variantCall';
 
 
 UPDATE public.analysis_schema
-SET  "schema"='{"type":"object","required":["experiment"],"properties":{"experiment":{"type":"object","required":["libraryStrategy"],"properties":{"aligned":{"type":["boolean","null"]},"pairedEnd":{"type":["boolean","null"]},"insertSize":{"type":["integer","null"]},"alignmentTool":{"type":["string","null"]},"libraryStrategy":{"enum":["WGS","WXS","RNA-Seq","ChIP-Seq","miRNA-Seq","Bisulfite-Seq","Validation","Amplicon","Other"],"type":"string"},"referenceGenome":{"type":["string","null"]},"fileTypes":{"enum":["FASTQ","BAM","CRAM"],"type":"string"}}}}}'::jsonb
-where name = 'sequencingRead';
+SET "schema"='{"type":"object","required":["experiment"],"properties":{"experiment":{"type":"object","required":["libraryStrategy"],"properties":{"aligned":{"type":["boolean","null"]},"fileTypes":{"oneOf":[{"$ref":"classpath:/schemas/analysis/analysisBase.json#/definitions/file/fileType"},{"type":"string"}]},"pairedEnd":{"type":["boolean","null"]},"insertSize":{"type":["integer","null"]},"alignmentTool":{"type":["string","null"]},"libraryStrategy":{"enum":["WGS","WXS","RNA-Seq","ChIP-Seq","miRNA-Seq","Bisulfite-Seq","Validation","Amplicon","Other"],"type":"string"},"referenceGenome":{"type":["string","null"]}}}}}'::jsonb
+WHERE "name"='sequencingRead';
