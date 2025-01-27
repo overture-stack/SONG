17c8ecd20d2dbce93a43be546f28cf273ac60eb5
Jon Eubank <joneubank@gmail.com>
2025-01-27 18:34:28 -0500

    Schema registrations sets options values correctly - when missing, uses value from previous schema or sets as empty (default) value - when set as empty array, replaces current value with empty array (removes optional validations) - sets options in get registration by version query

--- song-core/src/main/java/bio/overture/song/core/model/AnalysisTypeOptions.java
+++ song-core/src/main/java/bio/overture/song/core/model/AnalysisTypeOptions.java
@@ -1,7 +1,10 @@
 package bio.overture.song.core.model;
 
 import java.util.List;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
